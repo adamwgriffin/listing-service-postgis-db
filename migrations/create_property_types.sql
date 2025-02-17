5dec848b73b7f7f245c5f51af74faa087df40be9
Adam Griffin <ag@agriffin.me>
2025-02-16 20:45:17 -0800

    Created create_property_types migration

---
+++ migrations/create_property_types.sql
@@ -0,0 +1,15 @@
+CREATE TABLE property_types (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) UNIQUE NOT NULL,
+  label TEXT NOT NULL
+);
+
+INSERT INTO
+  property_types (name, label)
+VALUES
+  ('single-family', 'House'),
+  ('condo', 'Condo'),
+  ('townhouse', 'Townhouse'),
+  ('manufactured', 'Manufactured'),
+  ('land', 'Land'),
+  ('multi-family', 'Multi-Family');
