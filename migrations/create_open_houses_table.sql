e08d6ae38107e1a2945e7385a6594a2079ad2097
Adam Griffin <ag@agriffin.me>
2025-02-16 20:51:17 -0800

    Created open_houses table migration

---
+++ migrations/create_open_houses_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE open_houses (
+  id SERIAL PRIMARY KEY,
+  listing_id INTEGER NOT NULL,
+  start TIMESTAMP WITH TIME ZONE,
+  "end" TIMESTAMP WITH TIME ZONE,
+  comments VARCHAR(255),
+  FOREIGN KEY (listing_id) REFERENCES listings(id) ON DELETE CASCADE
+);
