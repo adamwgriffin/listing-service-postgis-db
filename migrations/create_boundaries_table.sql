e79c86b62752260c54800fdaf7842759e12bc893
Adam Griffin <ag@agriffin.me>
2025-06-06 16:37:37 -0700

    Formatting

--- migrations/create_boundaries_table.sql
+++ migrations/create_boundaries_table.sql
@@ -1,16 +1,17 @@
-CREATE TABLE boundaries (
-  id SERIAL PRIMARY KEY,
-  place_id TEXT,
-  name TEXT NOT NULL,
-  boundary_type_id INTEGER NOT NULL,
-  geom GEOMETRY(MULTIPOLYGON, 4326) NOT NULL,
-  FOREIGN KEY (boundary_type_id) REFERENCES boundary_types(id)
-);
+CREATE TABLE
+  boundaries (
+    id SERIAL PRIMARY KEY,
+    place_id TEXT,
+    name TEXT NOT NULL,
+    boundary_type_id INTEGER NOT NULL,
+    geom GEOMETRY (MULTIPOLYGON, 4326) NOT NULL,
+    FOREIGN KEY (boundary_type_id) REFERENCES boundary_types (id)
+  );
 
 COMMENT ON COLUMN boundaries.place_id IS 'The Google Maps geocoder place_id.';
 
 COMMENT ON COLUMN boundaries.geom IS 'The shape of the boundary. Using SRID 4326 ensures compatibility with GeoJSON (WGS 84)';
 
-CREATE INDEX boundaries_place_id_idx ON boundaries(place_id);
+CREATE INDEX boundaries_place_id_idx ON boundaries (place_id);
 
-CREATE INDEX boundaries_geom_idx ON boundaries USING GIST(geom);
+CREATE INDEX boundaries_geom_idx ON boundaries USING GIST (geom);
