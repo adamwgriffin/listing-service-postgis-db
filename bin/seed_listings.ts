0912c56b53b09947e0a11ce12e79f07bd5165c25
Adam Griffin <ag@agriffin.me>
2025-06-07 20:30:47 -0700

    Added code to insert additional listing data
    Adding photo galleries and open houses for each listing.

--- bin/seed_listings.ts
+++ bin/seed_listings.ts
@@ -2,13 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { Client } from 'pg';
 import dotenv from 'dotenv';
-import type { ListingData } from '../lib/listing';
-
-dotenv.config();
-
-const filePath = path.join(__dirname, '..', 'data', 'fremont_listings.json');
-
-const listings: ListingData[] = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+import type { ListingData, OpenHouse, PhotoGalleryImage } from '../lib/listing';
 
 const propertyTypeMapping = {
   'single-family': 1,
@@ -26,9 +20,8 @@ const propertyStatusMapping = {
   rented: 4
 };
 
-const values = listings
-  .map((l: ListingData) => {
-    return `
+function listingInsertValues(l: ListingData) {
+  return `
     (
       '${l.placeId}',
       '${l.address.line1}',
@@ -68,48 +61,107 @@ const values = listings
       ${propertyStatusMapping[l.status as keyof typeof propertyStatusMapping]},
       '${JSON.stringify(l.propertyDetails)}'::jsonb
     )
+  `;
+}
+
+function listingInsertQuery(listing: ListingData) {
+  return `
+    INSERT INTO
+      listings (
+        place_id,
+        address_line_1,
+        city,
+        "state",
+        zip,
+        geom,
+        neighborhood,
+        list_price,
+        sold_price,
+        listed_date,
+        sold_date,
+        "description",
+        beds,
+        baths,
+        sqft,
+        lot_size,
+        year_built,
+        rental,
+        waterfront,
+        view,
+        fireplace,
+        basement,
+        garage,
+        new_construction,
+        "pool",
+        air_conditioning,
+        property_type_id,
+        listing_status_id,
+        property_details
+      )
+    VALUES
+      ${listingInsertValues(listing)}
+    RETURNING
+      id;
+  `;
+}
+
+function photoGalleryInsertQuery(listingId: number) {
+  return `
+    INSERT INTO
+      photo_galleries (listing_id)
+    VALUES
+      (${listingId})
+    RETURNING
+      id;
+  `;
+}
+
+function photoGalleryImageInsertQuery(
+  photoGalleryId: number,
+  gallery: PhotoGalleryImage[]
+) {
+  const values = gallery
+    .map((image, index) => {
+      return `
+      (
+        ${photoGalleryId},
+        '${image.url}',
+        '${image.caption}',
+        ${index}
+      )
     `;
-  })
-  .join(', ');
+    })
+    .join(', ');
+  return `
+    INSERT INTO
+      photo_gallery_images (photo_gallery_id, url, caption, position)
+    VALUES
+      ${values};
+  `;
+}
 
-const sql = `
-  INSERT INTO
-    listings (
-      place_id,
-      address_line_1,
-      city,
-      "state",
-      zip,
-      geom,
-      neighborhood,
-      list_price,
-      sold_price,
-      listed_date,
-      sold_date,
-      "description",
-      beds,
-      baths,
-      sqft,
-      lot_size,
-      year_built,
-      rental,
-      waterfront,
-      view,
-      fireplace,
-      basement,
-      garage,
-      new_construction,
-      "pool",
-      air_conditioning,
-      property_type_id,
-      listing_status_id,
-      property_details
-    )
-  VALUES
-    ${values};
-`;
+function openHouseInsertQuery(listingId: number, openHouses: OpenHouse[]) {
+  const values = openHouses
+    .map((o) => {
+      return `
+      (
+        ${listingId},
+        '${o.start}',
+        '${o.end}',
+        ${o.comments ? "'" + o.comments + "'" : 'NULL'}
+      )
+    `;
+    })
+    .join(', ');
+  return `
+    INSERT INTO
+      open_houses (listing_id, start_time, end_time, comments)
+    VALUES
+      ${values};
+  `;
+}
 
-function createListingQuery(l: any) {}
+dotenv.config();
 
 const client = new Client({
   host: 'localhost',
@@ -120,8 +172,32 @@ const client = new Client({
 });
 
 try {
+  const filePath = path.join(__dirname, '..', 'data', 'fremont_listings.json');
+  const listings: ListingData[] = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
   await client.connect();
-  await client.query(sql);
+
+  for (const listing of listings) {
+    const listingRes = await client.query(listingInsertQuery(listing));
+    const listingId = listingRes.rows[0].id;
+    if (listing.photoGallery) {
+      const photoGalleryRes = await client.query(
+        photoGalleryInsertQuery(listingId)
+      );
+      const imageQuery = photoGalleryImageInsertQuery(
+        photoGalleryRes.rows[0].id,
+        listing.photoGallery
+      );
+      await client.query(imageQuery);
+    }
+    if (listing.openHouses?.length) {
+      const openHouseQuery = openHouseInsertQuery(
+        listingId,
+        listing.openHouses
+      );
+      await client.query(openHouseQuery);
+    }
+  }
   console.log('Listings seeded successfully.');
 } catch (err) {
   console.error('Error seeding listings:', err);
