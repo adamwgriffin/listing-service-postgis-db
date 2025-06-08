87777404ad33a9bf3d5197b5b4a5cfd6aa2a42b8
Adam Griffin <ag@agriffin.me>
2025-06-07 20:31:32 -0700

    Updated listing detail query

--- queries/listing_detail.sql
+++ queries/listing_detail.sql
@@ -17,28 +17,42 @@ SELECT
   l.sold_price,
   l.listed_date,
   l.sold_date,
+  EXTRACT(DAY FROM (NOW() - l.listed_date)) AS days_on_market,
   l.address_line_1,
   l.address_line_2,
   l.city,
   l.state,
   l.zip,
+  ST_Y (l.geom) AS latitude,
+  ST_X (l.geom) AS longitude,
   l.neighborhood,
   ls.label AS "status",
   pt.label AS property_type,
   l.description,
   l.beds,
   l.baths,
+  l.sqft,
+  l.lot_size,
+  l.year_built,
+  l.rental,
+  l.waterfront,
+  l.view,
+  l.fireplace,
+  l.basement,
+  l.garage,
+  l.new_construction,
+  l.pool,
+  l.air_conditioning,
   COALESCE(pg_agg.photo_gallery, '[]') AS photo_gallery,
   COALESCE(oh_agg.open_houses, '[]') AS open_houses,
-  ST_Y(l.geom) AS latitude,
-  ST_X(l.geom) AS longitude
+  l.property_details
 FROM
   listings l
   LEFT JOIN listing_statuses ls ON l.listing_status_id = ls.id
   LEFT JOIN property_types pt ON l.property_type_id = pt.id
   LEFT JOIN LATERAL (
     SELECT
-      jsonb_agg(pgi) AS photo_gallery
+      jsonb_agg(jsonb_build_object('url', pgi.url, 'caption', pgi.caption)) AS photo_gallery
     FROM
       photo_galleries pg
       JOIN photo_gallery_images pgi ON pg.id = pgi.photo_gallery_id
@@ -47,12 +61,12 @@ FROM
   ) AS pg_agg ON TRUE
   LEFT JOIN LATERAL (
     SELECT
-      jsonb_agg(oh) AS open_houses
+      jsonb_agg(jsonb_build_object('start', oh.start_time, 'end', oh.end_time, 'comments', oh.comments)) AS open_houses
     FROM
       open_houses oh
     WHERE
       oh.listing_id = l.id
-      AND oh.start_time >= CURRENT_DATE
+            AND oh.start_time >= CURRENT_DATE
   ) AS oh_agg ON TRUE
 WHERE
-  l.id = 12;
+  l.id = 18;
