59f0d101b1d10e838d107fe49618da1f3e90caa6
Adam Griffin <ag@agriffin.me>
2025-09-14 11:10:51 -0700

    Cleaning up the repo

--- queries/create_tables/8-create_open_houses_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE
-  open_houses (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    listing_id INTEGER NOT NULL,
-    start_time TIMESTAMP WITH TIME ZONE,
-    end_time TIMESTAMP WITH TIME ZONE,
-    "comments" TEXT,
-    FOREIGN KEY (listing_id) REFERENCES listings (id) ON DELETE CASCADE
-  );
